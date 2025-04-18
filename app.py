fc9dc32ccb46df3ede6ec6005ec21bd69ad9c6be
Rajath-H-M <157487808+Rajath-H-M@users.noreply.github.com>
2025-04-18 14:03:11 +0530

    Update app.py

--- app.py
+++ app.py
@@ -12,7 +12,7 @@ def load_data():
 
 df = load_data()
 
-st.title("📊 Customer Segmentation & Discount Strategies")
+st.title("📊 Customer Segmentation & Decision Strategies")
 
 segments = df['Segment'].unique()
 selected_segment = st.sidebar.selectbox("🔍 Select Customer Segment", segments)
